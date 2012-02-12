59aeac9be96c5f8090bd1d298fda64a34d63a474
Tim Jarrett <tim.jarrett@gmail.com>
2012-02-12 17:47:27 -0500

    Adding a single pin to the map works...

--- src/com/viapx/zefram/ZeframActivity.java
+++ src/com/viapx/zefram/ZeframActivity.java
@@ -8,24 +8,43 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import com.viapx.zefram.lib.*;
 
+import de.android1.overlaymanager.ManagedOverlay;
+import de.android1.overlaymanager.ManagedOverlayGestureDetector;
+import de.android1.overlaymanager.ManagedOverlayItem;
+import de.android1.overlaymanager.OverlayManager;
+import de.android1.overlaymanager.ZoomEvent;
+
+/**
+ * 
+ * @author tjarrett
+ *
+ * @see http://stackoverflow.com/questions/1678493/android-maps-how-to-long-click-a-map
+ * @see http://stackoverflow.com/questions/4646584/how-to-get-lat-and-long-on-touch-event-from-goole-map
+ * @see http://code.google.com/p/mapview-overlay-manager/source/browse/trunk/OverlayManager/src/de/android1/overlaymanager/OverlayManager.java
+ */
 public class ZeframActivity extends MapActivity
 {
     private MapView mapView;
-    
+
     private LocationManager locationManager;
-    
+
     private MapController mapController;
-    
+
     private MyLocationOverlay userLocationOverlay;
+    
+    private OverlayManager overlayManager;
 
     /** Called when the activity is first created. */
     @Override
@@ -33,133 +52,137 @@ public class ZeframActivity extends MapActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        //Get our map view
+
+        // Get our map view
         mapView = (MapView)findViewById(R.id.mapview);
         
-        //Get our controller
+        //Show the built in zoom controls
+        mapView.setBuiltInZoomControls(true);
+
+        // Get our controller
         mapController = mapView.getController();
         
-        //Let's lay over the user's current location
-        userLocationOverlay = new MyLocationOverlay(this, mapView);
-        
-        //When we first get a location fix, animate over to the user's location
-        userLocationOverlay.runOnFirstFix(new Runnable() {
+        //Get our overlay manager (see http://code.google.com/p/mapview-overlay-manager/)
+        overlayManager = new OverlayManager(getApplication(), mapView);
+        ManagedOverlay managedOverlay = overlayManager.createOverlay("zeframOverlay", getResources().getDrawable(R.drawable.pushpin_green));
+        managedOverlay.setOnOverlayGestureListener(new ManagedOverlayGestureDetector.OnOverlayGestureListener() {
+
             @Override
-            public void run()
+            public boolean onDoubleTap(MotionEvent arg0, ManagedOverlay arg1, GeoPoint arg2, ManagedOverlayItem arg3)
             {
-                Log.d("Tim", "Animated to location..."); 
-                mapController.animateTo(userLocationOverlay.getMyLocation());
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent event, ManagedOverlay overlay)
+            {
+                int x = (int)event.getX();
+                int y = (int)event.getY();
                 
-            }//end run
+                GeoPoint geoPoint = mapView.getProjection().fromPixels(x, y);
+                
+                overlay.createItem(geoPoint);
+                
+                
+                //Toast.makeText(getApplicationContext(), "LongPress incoming...!", Toast.LENGTH_SHORT).show();
+                // TODO Auto-generated method stub
+                
+            }
+
+            @Override
+            public void onLongPressFinished(MotionEvent arg0, ManagedOverlay arg1, GeoPoint arg2, ManagedOverlayItem arg3)
+            {
+                // TODO Auto-generated method stub
+                
+            }
+
+            @Override
+            public boolean onScrolled(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3, ManagedOverlay arg4)
+            {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean onSingleTap(MotionEvent arg0, ManagedOverlay arg1, GeoPoint arg2, ManagedOverlayItem arg3)
+            {
+                // TODO Auto-generated method stub
+                return false;
+            }
+
+            @Override
+            public boolean onZoom(ZoomEvent arg0, ManagedOverlay arg1)
+            {
+                // TODO Auto-generated method stub
+                return false;
+            }
             
         });
+        overlayManager.populate();
         
-        //Set the overlay to be included
-        mapView.getOverlays().add(userLocationOverlay);
-        
-        //Set a reasonable zoom level
+        // Set a reasonable zoom level
         mapController.setZoom(18);
         mapView.setClickable(true);
         mapView.setEnabled(true);
-        
+
+        // Let's lay over the user's current location
+        userLocationOverlay = new MyLocationOverlay(this, mapView);
+
+        // When we first get a location fix, animate over to the user's location
+        userLocationOverlay.runOnFirstFix(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Log.d("Tim", "Animated to location...");
+                mapController.animateTo(userLocationOverlay.getMyLocation());
+
+            }// end run
+
+        });
+
+        // Set the overlay to be included
+        mapView.getOverlays().add(userLocationOverlay);
+
         userLocationOverlay.enableMyLocation();
         
-        /*
+        //Get our drawable icon
+        Drawable marker = getResources().getDrawable(R.drawable.pushpin_green);
+        marker.setBounds(0, 0, marker.getIntrinsicWidth(), marker.getIntrinsicHeight());
         
-        //Acquire a reference to the system Location Manager
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        
+        //mapView.getOverlays().add(new GeoFenceOverlay(marker)); 
 
-        //Define a listener that responds to location updates
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location)
-            {
-                
-                int latitude = (int)(location.getLatitude()*1000000);
-                int longitude = (int)(location.getLongitude()*1000000);
-                
-                GeoPoint gp = new GeoPoint(latitude, longitude);
-                
-                mapController.animateTo(gp);
-                mapController.zoomIn();
-                
-                mapController.setZoom(6);
-                
-                locationManager.removeUpdates(this); 
-                
-                
-                Log.d("Tim", Float.toString(location.getAccuracy()));
-                
-                 
-                
-            }
+    }// end onCreate
 
-            @Override
-            public void onProviderDisabled(String arg0)
-            {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void onProviderEnabled(String arg0)
-            {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void onStatusChanged(String arg0, int arg1, Bundle arg2)
-            {
-                // TODO Auto-generated method stub
-                
-            }
-
-        };
-
-        // Register the listener with the Location Manager to receive location updates
-        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 10, locationListener);
-        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 10, locationListener);
-        
-        
-       
-        
-        
-        //Log.d("Tim", "Lat: " + location.getLatitude());
-        //Log.d("Tim", "Long: " + location.getLongitude());
-
-        //GeoPoint geoPoint = new GeoPoint((int)location.getLatitude()*1000000, (int)location.getLongitude()*1000000);
-        
-        
-
-        
-        Location lastKnown = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        Log.d("Tim", "Last Known: " + lastKnown.getLatitude() + "; " + lastKnown.getLongitude());
-        
-        LocationUtils.centerMapViewOnLocation(mapView, lastKnown);
-        
-        mapController.setZoom(18);*/
-        
-        //mapController.animateTo(geoPoint);
-        
-    }//end onCreate
-    
+    /* (non-Javadoc)
+     * @see com.google.android.maps.MapActivity#onResume()
+     */
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-        /*mSensorManager.registerListener(mRotateView,
-                SensorManager.SENSOR_ORIENTATION,
-                SensorManager.SENSOR_DELAY_UI);*/
         userLocationOverlay.enableMyLocation();
-    }
+        
+    }//end onResume
+
+    /* (non-Javadoc)
+     * @see com.google.android.maps.MapActivity#onPause()
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        userLocationOverlay.disableMyLocation();
+        
+    }//end onPause
 
     @Override
     protected boolean isRouteDisplayed()
     {
         // TODO Auto-generated method stub
         return false;
-    }
-    
+        
+    }//end isRouteDisplayed
+
 }
