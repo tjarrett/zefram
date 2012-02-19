a6c1562ea93bb1a0dd404020a1979b560e2fadcf
Tim Jarrett <tim.jarrett@gmail.com>
2012-02-19 12:23:56 -0500

    Got the database helper working

--- src/com/viapx/zefram/lib/db/DatabaseHelper.java
+++ src/com/viapx/zefram/lib/db/DatabaseHelper.java
@@ -16,28 +16,41 @@ import com.viapx.zefram.*;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper
 {
-    // name of the database file for your application -- change to something
-    // appropriate for your app
+    /**
+     * The name of the database
+     */
     private static final String DATABASE_NAME = "zefram.db";
 
-    // any time you make changes to your database objects, you may have to
-    // increase the database version
+    /**
+     * The version of the database (don't forget to increase this if database structure changes
+     */
     private static final int DATABASE_VERSION = 1;
 
+    /**
+     * Constructor
+     * @param context
+     */
     public DatabaseHelper(Context context)
     {
         super(context, DATABASE_NAME, null, DATABASE_VERSION, R.raw.ormlite_config);
-    }
+        
+    }//end DatabaseHelper
 
+    /**
+     * 
+     */
     @Override
-    public void onCreate(SQLiteDatabase arg0, ConnectionSource arg1)
+    public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource)
     {
         // TODO Auto-generated method stub
 
     }
 
+    /**
+     * 
+     */
     @Override
-    public void onUpgrade(SQLiteDatabase arg0, ConnectionSource arg1, int arg2, int arg3)
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion)
     {
         // TODO Auto-generated method stub
 
